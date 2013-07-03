785b68b3074a45a4742d438a997e87e48f0351a1
ForbesLindesay <forbes@lindesay.co.uk>
2013-07-03 18:06:38 +0100

    Update example

--- example/static/non-browserify.js
+++ example/static/non-browserify.js
@@ -1,7 +1,6 @@
 var hyperquest = require('hyperquest');
 var concat = require('concat-stream');
 hyperquest('/ajax')
-  .pipe(concat(function (err, res) {
-    if (err) throw err;
+  .pipe(concat(function (res) {
     console.log(res);
   }))
