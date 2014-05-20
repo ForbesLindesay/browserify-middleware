46e1da3354ed4b068e540b340906296274b747bd
Forbes Lindesay <forbes.lindesay@red-gate.com>
2014-05-20 10:35:14 +0100

    Fully remove magic path resolution
    [closes #21]

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+'use strict';
+
 var path = require('path');
 var normalize = path.normalize;
 var resolve = path.resolve;
 var dirname = path.dirname;
-var callsite = require('callsite');
 var stat = require('fs').statSync;
 
 exports = module.exports = browserify;
@@ -10,31 +11,12 @@ function browserify(path, options) {
   if (Array.isArray(path)) {
     return exports.modules(path, options);
   }
-  if (resolve(path) === normalize(path)) {
-    path = normalize(path);
-  } else {
-    var dir = directory();
-    options = exports.settings.normalize(options);
-    options.noParse = options.noParse.map(function (path) {
-      if (path[0] != '.') return path; //support `['jquery']` as well as `['./src/jquery.js']`
-      if (resolve(path) === normalize(path)) {
-        return normalize(path);
-      } else {
-        if (resolve(dir, path) !== resolve(path)) {
-          var e = new Error('Using relative paths is deprecated, use absolute paths via require.resolve(path).');
-          e.name = 'Warning';
-          console.warn(e.stack);
-        }
-        return resolve(dir, path);
-      }
-    })
-    if (resolve(dir, path) !== resolve(path)) {
-      var e = new Error('Using relative paths is deprecated, use absolute paths via require.resolve(path).');
-      e.name = 'Warning';
-      console.warn(e.stack);
-    }
-    path = resolve(dir, path);
-  }
+  path = resolve(path);
+  options = exports.settings.normalize(options);
+  options.noParse = options.noParse.map(function (path) {
+    if (path[0] != '.') return path; //support `['jquery']` as well as `['./src/jquery.js']`
+    return resolve(path);
+  });
   if (stat(path).isDirectory()) {
     return exports.directory(path, options);
   } else {
@@ -46,13 +28,3 @@ exports.file = require('./lib/file');
 exports.modules = require('./lib/modules');
 
 exports.settings = require('./lib/settings');
-
-function directory(exclude) {
-  var stack = callsite();
-  for (var i = 0; i < stack.length; i++) {
-    var filename = stack[i].getFileName();
-    if (filename !== __filename && (!exclude || (exclude.indexOf(filename) === -1)))
-      return dirname(filename);
-  }
-  throw new Error('Could not resolve directory');
-}
